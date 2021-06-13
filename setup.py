b50f4a3ba5a88bf1dcb5c3ab40916bde4a0f66d0
Zafar Shadman <36101944+zapcode@users.noreply.github.com>
2021-06-13 21:51:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(
-	name = 'add-py',         # How you named your package folder (MyLib)
+	name = 'add',         # How you named your package folder (MyLib)
 	packages = ['add'],   # Chose the same as "name"
 	version = '0.3',      # Start with a small number and increase it with every change you make
 	license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
